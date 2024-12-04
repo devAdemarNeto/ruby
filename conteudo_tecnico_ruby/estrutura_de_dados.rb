81ab2b7a4597e3beca1a098941dc965cf99b7d99
Ademar Neto <ademarnetodev@gmail.com>
2024-12-03 22:45:32 -0300

    first commit

---
+++ conteudo_tecnico_ruby/estrutura_de_dados.rb
@@ -0,0 +1,32 @@
+# Interger
+numero_de_macas1 = 4
+numero_de_macas2 = 7
+
+# Float
+altura = 1.76
+
+# String
+nome = "Neto"
+
+# Bolean
+verdadeiro = true
+falso = false
+
+# Symbol
+:variavel
+
+
+# Array -  lista de dados
+lista_de_compras = ['maca', 'pera', 'abobora']
+
+# Hash - estruturação dos dados
+
+dados_maca = {cor: 'vermelha', preco: 1.76, validade: true}
+
+
+# Range
+puts (0..4)
+
+# Set
+
+
